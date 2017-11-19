180b00c49961f4b9ad99ee843b14a00deeae3ec1
jkluio132465 <jkluio132465@gmail.com>
2017-11-20 03:23:50 +0800

    定義子視圖

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Laravel 快速入門 - 進階</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
