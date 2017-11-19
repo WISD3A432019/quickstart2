67cedbad7fd4cbabab82eae4644b6b7520e3ca29
jkluio132465 <jkluio132465@gmail.com>
2017-11-20 03:15:26 +0800

    控制器路由

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::auth();
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
